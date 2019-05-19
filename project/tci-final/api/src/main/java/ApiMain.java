fc003d84778dc73cd98930579bbe4d45e94e2dbf
Maria <mimi.andreeva@abv.bg>
2019-05-19 23:03:57 +0200

    Mapper and API Javadoc

--- project/tci-final/api/src/main/java/ApiMain.java
+++ project/tci-final/api/src/main/java/ApiMain.java
@@ -1,2 +1,57 @@
+import models.RequestInfo;
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
+import javax.xml.ws.Response;
+
 public class ApiMain {
+
+    LogicMain businessLogic;
+
+    /**
+     * Uses the businessLogic to get all scraped data from a base url
+     * and returns that data in JSON format.
+     * @param url
+     * @return
+     */
+    public Response getAll(String url) {
+        throw new NotImplementedException();
+    }
+
+    /**
+     * Uses the businessLogic to get a model with given type and extra data from a base url
+     * and returns that model in JSON format.
+     * @param url
+     * @param type
+     * @param extraInfo
+     * @return
+     */
+    public Response getOne(String url, String type, String extraInfo) {
+        throw new NotImplementedException();
+    }
+
+    /**
+     * Returns the information about the last request performed.
+     * If there is no such information, it returns code NotFound.
+     * @return
+     */
+    public Response getLastRequest() {
+        throw new NotImplementedException();
+    }
+
+    /**
+     * Writes the information about the performed request to a file.
+     * @param info
+     */
+    private void writeRequestInfoToFile(RequestInfo info) {
+        throw new NotImplementedException();
+    }
+
+    /**
+     * Reads from file where the information about the last request is stored
+     * and return it in the format of RequestInfo model.
+     * @return
+     */
+    private RequestInfo readFromFile() {
+        throw new NotImplementedException();
+    }
 }
--- project/tci-final/logic/src/main/java/mappers/ModelMapper.java
+++ project/tci-final/logic/src/main/java/mappers/ModelMapper.java
@@ -8,19 +8,38 @@ import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
 public class ModelMapper {
 
-    public IModel mapToModel(String data) {
+    /**
+     * Checks the category of the data provided and calls a method to map
+     * to a IModel instance accordingly. Returns an exception if no category is found.
+     * @param data
+     * @return
+     */
+    public IModel mapToModel(String data) { throw new NotImplementedException(); }
+
+    /**
+     * Maps data from the scraper to Book model.
+     * @param data
+     * @return
+     */
+    private Book mapToBook(String data) {
         throw new NotImplementedException();
     }
 
-    private void mapToBook(String data, Book model) {
-
+    /**
+     * Maps data from the scraper to Movie model.
+     * @param data
+     * @return
+     */
+    private Movie mapToMovie(String data) {
+        throw new NotImplementedException();
     }
 
-    private void mapToMovie(String data, Movie model) {
-
-    }
-
-    private void mapToMusic(String data, Music model) {
-
+    /**
+     * Maps data from the scraper to Music model.
+     * @param data
+     * @return
+     */
+    private Music mapToMusic(String data) {
+        throw new NotImplementedException();
     }
 }
