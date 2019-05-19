55a962950d052ba7fb678cd28353c387f2b4bbb1
Atanasov,Borislav B.S <b.atanasov@student.fontys.nl>
2019-05-19 22:40:54 +0200

    Scraper, ThreadService and all models defined

--- project/tci-final/logic/src/main/java/models/Book.java
+++ project/tci-final/logic/src/main/java/models/Book.java
@@ -1,4 +1,80 @@
 package models;
 
-public class Book {
+public class Book implements IModel{
+    private String name;
+    private String genre;
+    private String format;
+    private Integer year;
+    private String authors;
+    private String publisher;
+    private String isnb;
+
+    public Book(){}
+
+    public Book(String name, String genre, String format, Integer year, String authors, String publisher, String isnb){
+
+        this.name = name;
+        this.genre = genre;
+        this.format = format;
+        this.year = year;
+        this.authors = authors;
+        this.publisher = publisher;
+        this.isnb = isnb;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public String getFormat() {
+        return format;
+    }
+
+    public void setFormat(String format) {
+        this.format = format;
+    }
+
+    public Integer getYear() {
+        return year;
+    }
+
+    public void setYear(Integer year) {
+        this.year = year;
+    }
+
+    public String getAuthors() {
+        return authors;
+    }
+
+    public void setAuthors(String authors) {
+        this.authors = authors;
+    }
+
+    public String getPublisher() {
+        return publisher;
+    }
+
+    public void setPublisher(String publisher) {
+        this.publisher = publisher;
+    }
+
+    public String getIsnb() {
+        return isnb;
+    }
+
+    public void setIsnb(String isnb) {
+        this.isnb = isnb;
+    }
 }
--- project/tci-final/logic/src/main/java/models/Movie.java
+++ project/tci-final/logic/src/main/java/models/Movie.java
@@ -1,4 +1,80 @@
 package models;
 
-public class Movie {
+public class Movie implements IModel{
+    private String name;
+    private String genre;
+    private String format;
+    private Integer year;
+    private String director;
+    private String[] writers;
+    private String[] stars;
+
+    public Movie(){}
+
+    public Movie(String name, String genre, String format, Integer year, String director, String[] writers, String[] stars){
+
+        this.name = name;
+        this.genre = genre;
+        this.format = format;
+        this.year = year;
+        this.director = director;
+        this.writers = writers;
+        this.stars = stars;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public String getFormat() {
+        return format;
+    }
+
+    public void setFormat(String format) {
+        this.format = format;
+    }
+
+    public Integer getYear() {
+        return year;
+    }
+
+    public void setYear(Integer year) {
+        this.year = year;
+    }
+
+    public String getDirector() {
+        return director;
+    }
+
+    public void setDirector(String director) {
+        this.director = director;
+    }
+
+    public String[] getWriters() {
+        return writers;
+    }
+
+    public void setWriters(String[] writers) {
+        this.writers = writers;
+    }
+
+    public String[] getStars() {
+        return stars;
+    }
+
+    public void setStars(String[] stars) {
+        this.stars = stars;
+    }
 }
