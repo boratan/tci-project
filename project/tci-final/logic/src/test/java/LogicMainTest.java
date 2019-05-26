fe77107940571fb7faac56fe04d215a5261d47f3
Maria <mimi.andreeva@abv.bg>
2019-05-26 20:47:06 +0200

    another custom exception and test names for logic main class

--- project/tci-final/logic/src/test/java/LogicMainTest.java
+++ project/tci-final/logic/src/test/java/LogicMainTest.java
@@ -1,5 +1,71 @@
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class LogicMainTest {
 
+    @Test
+    public void getAllFromUrlReturnsInvalidArgumentExceptionIfUrlIsNull() {
+
+    }
+
+    @Test
+    public void getAllFromUrlInvokesCrawlUrlsMethodIfUrlIsNotNull() {
+
+    }
+
+    @Test
+    public void getAllFromUrlInvokesScrapeUrlsMethodIfUrlIsNotNull() {
+
+    }
+
+    @Test
+    public void getOneFromUrlReturnsInvalidArgumentExceptionIfUrlIsNull() {
+
+    }
+
+    @Test
+    public void getOneFromUrlInvokesCrawlUrlsMethodIfUrlIsNotNull() {
+
+    }
+
+    @Test
+    public void getOneFromUrlInvokesScrapeUrlsMethodIfUrlIsNotNull() {
+
+    }
+
+    @Test
+    public void headUrlReturnsInvalidArgumentExceptionIfStartUrlIsNull() {
+
+    }
+
+    @Test
+    public void headUrlReturnsSetOfThePassedUrlIfItIsNotNull() {
+
+    }
+
+    @Test
+    public void crawlUrlsReturnsInvalidArgumentExceptionIfStartUrlIsNull() {
+
+    }
+
+    @Test
+    public void crawlUrlsInvokesHeadURLMethodIfUrlIsNotNull() {
+
+    }
+
+    @Test
+    public void crawlUrlsInvokesCrawlerCrawlMethodIfUrlIsNotNull() {
+
+    }
+
+    @Test
+    public void scrapeUrlsInvokesThreadServiceScrapeMethodIfDistinctUrlsIsNotEmpty() {
+
+    }
+
+    @Test
+    public void scrapeUrlsReturnsIModelNotFoundExceptionIfDistinctUrlsIsEmpty() {
+
+    }
 }
