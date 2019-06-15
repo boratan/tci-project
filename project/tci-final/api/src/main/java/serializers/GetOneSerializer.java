d72a070472bfa60b7dc78012bc10331223094f3e
Maria <mimi.andreeva@abv.bg>
2019-06-15 19:42:51 +0200

    GetOneSerializer implementation

--- project/tci-final/api/src/main/java/serializers/GetOneSerializer.java
+++ project/tci-final/api/src/main/java/serializers/GetOneSerializer.java
@@ -6,19 +6,21 @@ import models.GetOne;
 public class GetOneSerializer implements IGenericSerializer<GetOne> {
     private Gson gson;
 
-    public Gson getGson() {
-        return gson;
-    }
-
     public GetOneSerializer() { gson = new Gson(); }
 
     @Override
     public String serializeToJson(GetOne object) {
-        return null;
+        if(object == null) {
+            throw new IllegalArgumentException();
+        }
+        return gson.toJson(object);
     }
 
     @Override
     public GetOne deserializeFromJson(String json) {
-        return null;
+        if(json == null || json == "") {
+            throw new IllegalArgumentException();
+        }
+        return gson.fromJson(json, GetOne.class);
     }
 }
