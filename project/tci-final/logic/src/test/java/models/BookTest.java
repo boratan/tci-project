14c6b883fc0bfccc1b204617db1a9207340b3879
veso <i341777@fhict.nl>
2019-06-15 22:02:50 +0200

    Book Tests

--- project/tci-final/logic/src/test/java/models/BookTest.java
+++ project/tci-final/logic/src/test/java/models/BookTest.java
@@ -1,74 +1,125 @@
 package models;
 
-import net.bytebuddy.build.ToStringPlugin;
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
-import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class BookTest {
 
-    @Test
-    public void BookNameIsNotNullWhenConstructorIsInvoked(){}
+    private Book book;
+
+    @Before
+    public void setUp(){
+        List<String> authors = new ArrayList<>();
+        authors.add("Douglas Adams");
+        book = new Book(
+                "The Hitchhiker's Guide to the Galaxy",
+                "Science Fiction",
+                "Hardcover",
+                1978,
+                    authors,
+                "Hermes",
+                "978-0201485669"
+        );
+    }
 
     @Test
-    public void BookGenreIsNotNullWhenConstructorIsInvoked(){}
+    public void BookNameIsNotNullWhenConstructorIsInvoked(){
+        Assert.assertNotNull(book.getName());
+    }
 
     @Test
-    public void BookFormatIsNotNullWhenConstructorIsInvoked(){}
+    public void BookGenreIsNotNullWhenConstructorIsInvoked(){
+        Assert.assertNotNull(book.getGenre());
+    }
 
     @Test
-    public void BookYearIsNotNullWhenConstructorIsInvoked(){}
+    public void BookFormatIsNotNullWhenConstructorIsInvoked(){
+        Assert.assertNotNull(book.getFormat());
+    }
 
     @Test
-    public void BookAuthorsIsNotNullWhenConstructorIsInvoked(){}
+    public void BookYearIsNotNullWhenConstructorIsInvoked(){
+        Assert.assertNotNull(book.getYear());
+    }
 
     @Test
-    public void BookPublisherIsNotNullWhenConstructorIsInvoked(){}
+    public void BookAuthorsIsNotNullWhenConstructorIsInvoked(){
+        Assert.assertNotNull(book.getAuthors());
+    }
 
     @Test
-    public void BookIsbnIsNotNullWhenConstructorIsInvoked(){}
+    public void BookPublisherIsNotNullWhenConstructorIsInvoked(){
+        Assert.assertNotNull(book.getPublisher());
+    }
 
     @Test
-    public void BookNameThrowsExceptionIfTheGivenValueIsInvalid(){}
+    public void BookIsbnIsNotNullWhenConstructorIsInvoked(){
+        Assert.assertNotNull(book.getIsbn());
+    }
 
-    @Test
-    public void BookGenreThrowsExceptionIfTheGivenValueIsInvalid(){}
+    @Test (expected = IllegalArgumentException.class)
+    public void BookNameThrowsExceptionIfTheGivenValueIsNull(){
+        book.setName(null);
+    }
 
-    @Test
-    public void BookFormatThrowsExceptionIfTheGivenValueIsInvalid(){}
+    @Test (expected = IllegalArgumentException.class)
+    public void BookNameThrowsExceptionIfTheGivenValueIsEmpty(){
+        book.setName("");
+    }
 
-    @Test
-    public void BookYearThrowsExceptionIfTheGivenValueIsInvalid(){}
+    @Test (expected = IllegalArgumentException.class)
+    public void BookGenreThrowsExceptionIfTheGivenValueIsNull(){
+        book.setGenre(null);
+    }
 
-    @Test
-    public void BookAuthorsThrowsExceptionIfTheGivenValueIsInvalid(){}
+    @Test (expected = IllegalArgumentException.class)
+    public void BookGenreThrowsExceptionIfTheGivenValueIsEmpty(){
+        book.setGenre("");
+    }
 
-    @Test
-    public void BookPublisherThrowsExceptionIfTheGivenValueIsInvalid(){}
+    @Test (expected = IllegalArgumentException.class)
+    public void BookFormatThrowsExceptionIfTheGivenValueIsNull(){
+        book.setFormat(null);
+    }
 
-    @Test
-    public void BookIsbnThrowsExceptionIfTheGivenValueIsInvalid(){}
+    @Test (expected = IllegalArgumentException.class)
+    public void BookFormatThrowsExceptionIfTheGivenValueIsEmpty(){
+        book.setFormat("");
+    }
 
-    @Test
-    public void BookNameIsSetAsDesired(){}
+    @Test (expected = IllegalArgumentException.class)
+    public void BookYearThrowsExceptionIfTheGivenValueIsNegative(){
+        book.setYear(-5);
+    }
 
-    @Test
-    public void BookGenreIsSetAsDesired(){}
+    @Test (expected = IllegalArgumentException.class)
+    public void BookAuthorsThrowsExceptionIfTheGivenValueIsNull(){
+        book.setAuthors(null);
+    }
 
-    @Test
-    public void BookFormatIsSetAsDesired(){}
+    @Test (expected = IllegalArgumentException.class)
+    public void BookAuthorsThrowsExceptionIfTheGivenValueIsEmpty(){
+        List<String> testAuthors = Collections.emptyList();
+        book.setAuthors(testAuthors);
+    }
 
-    @Test
-    public void BookYearIsSetAsDesired(){}
+    @Test (expected = IllegalArgumentException.class)
+    public void BookPublisherThrowsExceptionIfTheGivenValueIsNull(){
+        book.setPublisher(null);
+    }
 
-    @Test
-    public void BookAuthorsIsSetAsDesired(){}
+    @Test (expected = IllegalArgumentException.class)
+    public void BookPublisherThrowsExceptionIfTheGivenValueIsEmpty(){
+        book.setPublisher("");
+    }
 
-    @Test
-    public void BookPublisherIsSetAsDesired(){}
-
-    @Test
-    public void BookIsbnIsSetAsDesired(){}
-
-    @Test
-    public void BookIsbnOnlyContainsNumberAndUnderScores(){}
+    @Test (expected = IllegalArgumentException.class)
+    public void SetBookIsbnThrowsIllegalArgumentExceptionIfStringIsNotOnlyNumberAndMinus(){
+        book.setisbn("Incorrect");
+    }
 }
