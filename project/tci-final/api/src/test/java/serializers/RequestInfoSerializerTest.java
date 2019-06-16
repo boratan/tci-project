0bf90e6af39abe80628ceb202c338f66d8787547
Atanasov,Borislav B.S <b.atanasov@student.fontys.nl>
2019-06-16 13:30:30 +0200

    RequestInfoSerializer tests

--- project/tci-final/api/src/test/java/serializers/RequestInfoSerializerTest.java
+++ project/tci-final/api/src/test/java/serializers/RequestInfoSerializerTest.java
@@ -1,39 +1,44 @@
 package serializers;
 
 import com.sun.javaws.exceptions.InvalidArgumentException;
+import models.RequestInfo;
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
-
 import static org.junit.Assert.*;
 
 public class RequestInfoSerializerTest {
+    private RequestInfoSerializer requestInfoSerializer;
 
-    @Test
-    public void afterConstructionGsonObjectCanBeReturned() {
-
-    }
-
-    @Test
-    public void serializeToJsonReturnsValidString() {
-
+    @Before
+    public void setUp(){
+        requestInfoSerializer = new RequestInfoSerializer();
     }
 
     @Test(expected = InvalidArgumentException.class)
     public void serializeToJsonReturnsInvalidArgumentExceptionIfRequestInfoIsNull() {
-
+        requestInfoSerializer.serializeToJson(null);
     }
 
     @Test
-    public void deserializeFromJsonReturnsRequestInfoThatIsNotNull() {
+    public void deserializeFromJsonReturnsRequestInfo() {
+        String json = "{'id':3, 'time':153255, 'pagesExplored':10, 'uniquePagesExplored':5, 'searchDepth':2}";
+        RequestInfo requestInfo = new RequestInfo(10, 5, 2);
+        requestInfo.setId(3);
+        requestInfo.setTime(153255);
 
+        Assert.assertEquals(requestInfo, requestInfoSerializer.deserializeFromJson(json));
     }
 
     @Test(expected = InvalidArgumentException.class)
     public void deserializeFromJsonReturnsInvalidArgumentExceptionIfStringIsNull() {
+        requestInfoSerializer.deserializeFromJson(null);
 
     }
 
     @Test(expected = InvalidArgumentException.class)
     public void deserializeFromJsonReturnsInvalidArgumentExceptionIfStringIsEmpty() {
-
+        String json = "";
+        requestInfoSerializer.deserializeFromJson(json);
     }
 }
