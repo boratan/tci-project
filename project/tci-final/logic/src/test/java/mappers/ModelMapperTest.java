5ad7fc5ce04f139b276cb563591e30ecfc92e8a6
Maria <mimi.andreeva@abv.bg>
2019-06-16 18:02:42 +0200

    ModelMapper tests

--- project/tci-final/logic/src/test/java/mappers/ModelMapperTest.java
+++ project/tci-final/logic/src/test/java/mappers/ModelMapperTest.java
@@ -1,63 +1,248 @@
 package mappers;
 
+import helper.ScrapedDataDoesNotContainIModelException;
+import models.Book;
+import models.IModel;
+import models.Movie;
+import models.Music;
+import org.jsoup.nodes.Element;
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
-
+/**
+ * Author: M. Andreeva
+ */
 public class ModelMapperTest {
 
-    @Test
-    public void mapToBookReturnsNotNullBookIfElementPassedActuallyHasBook() {
+    private ModelMapper mapper;
+    private Element bookElement;
+    private Element movieElement;
+    private Element musicElement;
 
+    @Before
+    public void setUp() {
+        mapper = new ModelMapper();
+
+        arrangeBook();
+        arrangeMovie();
+        arrangeMusic();
     }
 
-    @Test
-    public void mapToBookReturnsInvalidArgumentExceptionIfElementIsNull() {
+    /**
+     * Populated the bookElement with proper html structure.
+     */
+    private void arrangeBook() {
+        bookElement = new Element("div");
+        bookElement.addClass("media-details");
+        bookElement.appendElement("h1").text("name");
+
+        Element tbody = bookElement.appendElement("table").appendElement("tbody");
+        Element row = tbody.appendElement("tr");
+        row.appendElement("th").text("Category");
+        row.appendElement("td").text("Books");
+
+        row = tbody.appendElement("tr");
+        row.appendElement("th").text("Genre");
+        row.appendElement("td").text("genre");
+
+        row = tbody.appendElement("tr");
+        row.appendElement("th").text("Format");
+        row.appendElement("td").text("format");
+
+        row = tbody.appendElement("tr");
+        row.appendElement("th").text("Year");
+        row.appendElement("td").text("1925");
+
+        row = tbody.appendElement("tr");
+        row.appendElement("th").text("Authors");
+        row.appendElement("td").text("authors");
+
+        row = tbody.appendElement("tr");
+        row.appendElement("th").text("Publisher");
+        row.appendElement("td").text("publisher");
+
+        row = tbody.appendElement("tr");
+        row.appendElement("th").text("ISBN");
+        row.appendElement("td").text("isbn");
 
     }
+    /**
+     * Populated the movieElement with proper html structure.
+     */
+    private void arrangeMovie() {
+        movieElement = new Element("div");
+        movieElement.addClass("media-details");
+        movieElement.appendElement("h1").text("name");
 
-    @Test
-    public void mapToBookReturnsScrapedDataDoesNotContainIModelExceptionIfDataHasNoBook() {
+        Element tbody = movieElement.appendElement("table").appendElement("tbody");
+        Element row = tbody.appendElement("tr");
+        row.appendElement("th").text("Category");
+        row.appendElement("td").text("Movies");
 
+        row = tbody.appendElement("tr");
+        row.appendElement("th").text("Genre");
+        row.appendElement("td").text("genre");
+
+        row = tbody.appendElement("tr");
+        row.appendElement("th").text("Format");
+        row.appendElement("td").text("format");
+
+        row = tbody.appendElement("tr");
+        row.appendElement("th").text("Year");
+        row.appendElement("td").text("1925");
+
+        row = tbody.appendElement("tr");
+        row.appendElement("th").text("Director");
+        row.appendElement("td").text("director");
+
+        row = tbody.appendElement("tr");
+        row.appendElement("th").text("Writers");
+        row.appendElement("td").text("writer1, writer2");
+
+        row = tbody.appendElement("tr");
+        row.appendElement("th").text("Stars");
+        row.appendElement("td").text("Star1, Star2");
+    }
+    /**
+     * Populated the musicElement with proper html structure.
+     */
+    private void arrangeMusic() {
+        musicElement = new Element("div");
+        musicElement.addClass("media-details");
+        musicElement.appendElement("h1").text("name");
+
+        Element tbody = musicElement.appendElement("table").appendElement("tbody");
+        Element row = tbody.appendElement("tr");
+        row.appendElement("th").text("Category");
+        row.appendElement("td").text("Music");
+
+        row = tbody.appendElement("tr");
+        row.appendElement("th").text("Genre");
+        row.appendElement("td").text("genre");
+
+        row = tbody.appendElement("tr");
+        row.appendElement("th").text("Format");
+        row.appendElement("td").text("format");
+
+        row = tbody.appendElement("tr");
+        row.appendElement("th").text("Year");
+        row.appendElement("td").text("1925");
+
+        row = tbody.appendElement("tr");
+        row.appendElement("th").text("Artist");
+        row.appendElement("td").text("artist");
     }
 
-    @Test
-    public void mapToMovieReturnsNotNullBookIfElementPassedActuallyHasMovie() {
 
+    /**
+     * Calls mapToModel method of the Model Mapper with proper book element.
+     * Expected is an IModel which is instance of Book class.
+     */
+    @Test
+    public void mapToModelReturnsNotNullBookIfElementPassedActuallyHasBook() {
+        //arrange is in setUp
+
+        //act
+        try {
+            IModel actual = mapper.mapToModel(bookElement);
+            // assert
+            Assert.assertTrue(actual instanceof Book);
+        } catch (ScrapedDataDoesNotContainIModelException e) {
+            Assert.fail();
+        }
     }
 
+    /**
+     * Calls mapToModel method of the Model Mapper with proper movie element.
+     * Expected is an IModel which is instance of Movie class.
+     */
     @Test
-    public void mapToMovieReturnsInvalidArgumentExceptionIfElementIsNull() {
+    public void mapToModelReturnsNotNullMovieIfElementPassedActuallyHasMovie() {
+        //arrange is in setUp
 
+        //act
+        try {
+            IModel actual = mapper.mapToModel(movieElement);
+            // assert
+            Assert.assertTrue(actual instanceof Movie);
+        } catch (ScrapedDataDoesNotContainIModelException e) {
+            Assert.fail();
+        }
     }
 
+    /**
+     * Calls mapToModel method of the Model Mapper with proper music element.
+     * Expected is an IModel which is instance of Music class.
+     */
     @Test
-    public void mapToMovieReturnsScrapedDataDoesNotContainIModelExceptionIfDataHasNoMovie() {
+    public void mapToModelReturnsNotNullMusicIfElementPassedActuallyHasMusic() {
+        //arrange is in setUp
 
+        //act
+        try {
+            IModel actual = mapper.mapToModel(musicElement);
+            // assert
+            Assert.assertTrue(actual instanceof Music);
+        } catch (ScrapedDataDoesNotContainIModelException e) {
+            Assert.fail();
+        }
     }
 
-    @Test
-    public void mapToMusicReturnsNotNullBookIfElementPassedActuallyHasMusic() {
+    /**
+     * Calls mapToModel method of the Model Mapper with null value.
+     * Expected is an IllegalArgumentException.
+     */
+    @Test(expected = IllegalArgumentException.class)
+    public void mapToModelThrowsIllegalArgumentExceptionIfElementIsNull() throws ScrapedDataDoesNotContainIModelException {
+        //arrange
+        Element data = null;
 
+        //act
+        IModel actual = mapper.mapToModel(data);
+
+        // assert is the expected exception
     }
 
-    @Test
-    public void mapToMusicReturnsInvalidArgumentExceptionIfElementIsNull() {
+    /**
+     * Calls mapToModel method of the Model Mapper with book element with all details except Category.
+     * Expected is an ScrapedDataDoesNotContainIModelException.
+     */
+    @Test(expected = ScrapedDataDoesNotContainIModelException.class)
+    public void mapToModelThrowsScrapedDataDoesNotContainIModelExceptionIfElementHasNoCategory() throws ScrapedDataDoesNotContainIModelException {
+        //arrange
+        Element bookElement = new Element("div");
+        bookElement.addClass("media-details");
+        bookElement.appendElement("h1").text("name");
 
-    }
+        Element tbody = bookElement.appendElement("table").appendElement("tbody");
+        Element row = tbody.appendElement("tr");
+        row.appendElement("th").text("Genre");
+        row.appendElement("td").text("genre");
 
-    @Test
-    public void mapToMusicReturnsScrapedDataDoesNotContainIModelExceptionIfDataHasNoMusic() {
+        row = tbody.appendElement("tr");
+        row.appendElement("th").text("Format");
+        row.appendElement("td").text("format");
 
-    }
+        row = tbody.appendElement("tr");
+        row.appendElement("th").text("Year");
+        row.appendElement("td").text("1925");
 
-    @Test
-    public void mapToModelReturnsInvalidArgumentExceptionIfElementIsNull() {
+        row = tbody.appendElement("tr");
+        row.appendElement("th").text("Authors");
+        row.appendElement("td").text("authors");
 
-    }
+        row = tbody.appendElement("tr");
+        row.appendElement("th").text("Publisher");
+        row.appendElement("td").text("publisher");
 
-    @Test
-    public void mapToModelReturnsScrapedDataDoesNotContainIModelExceptionIfDataHasNoCategory() {
+        row = tbody.appendElement("tr");
+        row.appendElement("th").text("ISBN");
+        row.appendElement("td").text("isbn");
 
+        //act
+        IModel actual = mapper.mapToModel(bookElement);
+
+        // assert is the expected exception
     }
 }
